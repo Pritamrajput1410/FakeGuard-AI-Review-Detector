a4c2c73095020d57e71f86e3b77c05ccd07d077b
Pritam <pritamrajput140@gmail.com>
2026-07-14 07:56:10 +0530

    Auto-download NLTK resources for deployment environments

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,10 @@
 import re
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
+import nltk
 
+nltk.download('stopwords', quiet=True)
+nltk.download('wordnet', quiet=True)
 lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
 
@@ -11,4 +14,3 @@ def clean_text(text):
     words = text.split()
     words = [lemmatizer.lemmatize(word) for word in words if word not in stop_words]
     return ' '.join(words)
-    
